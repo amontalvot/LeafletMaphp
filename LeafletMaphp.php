c7894c0d5127c1c328a9b761ffa13206a0643d42
unknown <aaron.montalvo@uah.es>
2022-03-01 12:36:45 +0100

    Several text options added. Fixed minor bugs

--- LeafletMaphp.php
+++ LeafletMaphp.php
@@ -1,6 +1,6 @@
 <?php
 /*
-LeafletMaphp class, ver. 0.9
+LeafletMaphp class, ver. 1.0
 Copyright 2022 Aaron Montalvo
 
 This program is free software; you can redistribute it and/or modify
@@ -26,22 +26,29 @@ Requirements:
 class LeafletMaphpException extends Exception {};
 
 class LeafletMaphp {
+    const MARKER = 0;
+    const CIRCLE = 1;
+    const POLYGON = 2;
+
     private $div_id;
     private $div_height;
+    private $div_style;
     private $div_width;
-    private $lat = '';
-    private $lon = '';
-    private $zoom = '';
-    private $bounds = '';
+    private $lat = NULL;
+    private $lon = NULL;
+    private $zoom = 15;
+    private $bounds = NULL;
     private $markers = [];
     private $circles = [];
     private $polygons = [];
     private $geoJSONs = [];
+    private $onClickFunText = '';
 
-    function __construct(string $id='map', int $height = 300, int $width = 300) {
+    function __construct(string $id='map', int $height = 300, int $width = 300, string $style='') {
         $this->div_id = $id;
         $this->div_height = $height;
         $this->div_width = $width;
+        $this->div_style = $style;
     }
     function showHeadTags () : string {
         return "\t<link rel='stylesheet' href='https://unpkg.com/leaflet@1.7.1/dist/leaflet.css' integrity='sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==' crossorigin=''/>
@@ -49,44 +56,127 @@ class LeafletMaphp {
     <style>#{$this->div_id} { height: {$this->div_height}px; width: {$this->div_width}px }</style>\n";
     }
 
-    function setCenter(float $lat, float $lon, array $bounds, int $zoom=17) {
-        if(count($bounds) != 4) 
-            throw new LeafletMaphpException('Bounds array != 4');
+    function setCenter(float $lat, float $lon, array $bounds=NULL, int $zoom=NULL) {
         $this->lat = $lat;
         $this->lon = $lon;
-        $this->bounds = $bounds;
-        $this->zoom = $zoom;
+        if($zoom != NULL)
+            $this->zoom = $zoom;
+        if($bounds != NULL) {
+            if(count($bounds) != 4) 
+                throw new LeafletMaphpException('Bounds array != 4');
+            $this->bounds = $bounds;
+        }
     }
     
-    function addMarker (float $lat, float $lon, string $toolTip='', string $onClick='') {
+    function addMarker (float $lat, float $lon) : int {
         $marker['lat'] = $lat;
         $marker['lon'] = $lon;
-        if($toolTip != '') {
-            $marker['toolTip'] = $toolTip;
-        }
-        if($onClick != '') {
-            $marker['onClick'] = $onClick;
-        }
         array_push($this->markers, $marker);
+        return (count($this->markers)-1);
     }
 
-    function addCircle(float $lat, float $lon, string $color='red') {
+    function addCircle(float $lat, float $lon, string $color=NULL, float $radius=NULL) : int  {
         $circle['lat'] = $lat;
         $circle['lon'] = $lon;
-        $circle['color'] = $color;
+        if($color != NULL) $circle['color'] = $color;
+        if($radius != NULL) $circle['radius'] = $radius;
         array_push($this->circles, $circle);
+
+        //if circles are written and there was no map initialization, getBounds function won't work
+        //if no center set, force first circle as center
+        if((count($this->circles) == 1) && ($this->lat == NULL) && ($this->lon == NULL)) {
+            $this->lat = $lat;
+            $this->lon = $lon;
+        }
+        return (count($this->circles)-1);
     }
 
-    function addPolygon (array $polydata, string $color='blue') {
+    function addPolygon (array $polydata, string $color=NULL) : int  {
         $polygon['data'] = $polydata;
-        $polygon['color'] = $color;
+        if($color != NULL) $polygon['color'] = $color;
         array_push($this->polygons, $polygon);
+        return (count($this->polygons)-1);
     }
 
-    function addGeoJSON(string $geoJSONdata, string $color='blue') {
+    function addMultipolygon (array $multipolydata, string $color=NULL) : int  {
+        $polygon['multi'] = $multipolydata;
+        if($color != NULL) $polygon['color'] = $color;
+        array_push($this->polygons, $polygon);
+        return (count($this->polygons)-1);
+}
+    
+    function addTooltip (int $element_type, int $element_id, string $toolTip) {
+        switch($element_type) {
+            case self::MARKER:
+                if(!isset($this->markers[$element_id])) throw new LeafletMaphpException('Wrong marker ID');
+                $this->markers[$element_id]['toolTip'] = $toolTip;
+                break;
+            case self::CIRCLE:
+                if(!isset($this->circles[$element_id])) throw new LeafletMaphpException('Wrong circle ID');
+                $this->circles[$element_id]['toolTip'] = $toolTip;
+                break;
+            case self::POLYGON:
+                if(!isset($this->polygons[$element_id])) throw new LeafletMaphpException('Wrong polygon ID');
+                $this->polygons[$element_id]['toolTip'] = $toolTip;
+                break;
+            default:
+                throw new LeafletMaphpException('Wrong element type');
+                break;
+        }
+    }
+    
+    function addPopUp (int $element_type, int $element_id, string $popUp) {
+        switch($element_type) {
+            case self::MARKER:
+                if(!isset($this->markers[$element_id])) throw new LeafletMaphpException('Wrong marker ID');
+                $this->markers[$element_id]['popUp'] = $popUp;
+                break;
+            case self::CIRCLE:
+                if(!isset($this->circles[$element_id])) throw new LeafletMaphpException('Wrong circle ID');
+                $this->circles[$element_id]['popUp'] = $popUp;
+                break;
+            case self::POLYGON:
+                if(!isset($this->polygons[$element_id])) throw new LeafletMaphpException('Wrong polygon ID');
+                $this->polygons[$element_id]['popUp'] = $popUp;
+                break;
+            default:
+                throw new LeafletMaphpException('Wrong element type');
+                break;
+        }
+    }
+    
+    function addOnClickText (int $element_type, int $element_id, string $onClick) {
+        switch($element_type) {
+            case self::MARKER:
+                if(!isset($this->markers[$element_id])) throw new LeafletMaphpException('Wrong marker ID');
+                $this->markers[$element_id]['onClick'] = $onClick;
+                break;
+            case self::CIRCLE:
+                if(!isset($this->circles[$element_id])) throw new LeafletMaphpException('Wrong circle ID');
+                $this->circles[$element_id]['onClick'] = $onClick;
+                break;
+            case self::POLYGON:
+                if(!isset($this->polygons[$element_id])) throw new LeafletMaphpException('Wrong polygon ID');
+                $this->polygons[$element_id]['onClick'] = $onClick;
+                break;
+            default:
+                throw new LeafletMaphpException('Wrong element type');
+                break;
+        }
+    }
+
+    function addGeoJSON(string $geoJSONdata, string $color=NULL) {
         $geoJSON['data'] = $geoJSONdata;
-        $geoJSON['color'] = $color;
+        if($color != NULL) $geoJSONdata['color'] = $color;
         array_push($this->geoJSONs, $geoJSON);
+        
+        //if geosjon data are written and there was no map initialization, getBounds function won't work
+        //if no center set, force first geojson first coordinate as center
+        if((count($this->geoJSONs) == 1) && ($this->lat == NULL) && ($this->lon == NULL)) {
+            $decoded_data = json_decode($geoJSONdata, true);
+            $this->lat = $decoded_data['geometries'][0]['coordinates'][0][0][0][1];
+            $this->lon = $decoded_data['geometries'][0]['coordinates'][0][0][0][0];
+        }
     }
 
     function showOnClickDiv() : string {
@@ -94,54 +184,109 @@ class LeafletMaphp {
     }
 
     function show() : string {
-        $scriptText = "var map = L.map('{$this->div_id}');\nL.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors', maxZoom: 18 }).addTo(map);\n";
-
-        if((is_array($this->bounds)) && (count($this->bounds) == 4)) {
-            $scriptText .= "map.fitBounds([[{$this->bounds[0]}, {$this->bounds[2]}], [{$this->bounds[1]}, {$this->bounds[3]}]]);\n";
-        }
-
-        if((count($this->markers) > 1) || (count($this->circles) > 1) || (count($this->polygons) != 0) || (count($this->geoJSONs) != 0)) {
+        if((count($this->markers) == 0) && (count($this->circles) == 0) && (count($this->polygons) == 0) && (count($this->geoJSONs) == 0)) {
+            if(($this->lat == NULL) && ($this->lon == NULL)) {
+                throw new LeafletMaphpException('No items added nor center set: Map is inviewable');
+            }
+        } else {
             $drawnItems = "var drawnItems = new L.FeatureGroup([";
         }
 
-        $onClickFunText = '';
+        $scriptText = "var map = L.map('{$this->div_id}');\nL.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors', maxZoom: 18 }).addTo(map);\n";
+        
+        if((is_array($this->bounds)) && (count($this->bounds) == 4)) {
+            $scriptText .= "map.fitBounds([[{$this->bounds[0]}, {$this->bounds[2]}], [{$this->bounds[1]}, {$this->bounds[3]}]]);\n";
+        }
+        else if(($this->lat != NULL) && ($this->lon != NULL)){
+            $scriptText .= "map.setView([{$this->lat}, {$this->lon}], {$this->zoom});\n";
+        }
+        if((count($this->markers) != 0) || (count($this->circles) != 0) || (count($this->polygons) != 0) || (count($this->geoJSONs) != 0)) {
+            $drawnItems = "var drawnItems = new L.FeatureGroup([";
+        }
+
         for($i=0; $i<count($this->markers); ++$i) {
             $markertext = "var marker$i = L.marker([{$this->markers[$i]['lat']}, {$this->markers[$i]['lon']}]";
-            
             if(isset($this->markers[$i]['onClick'])) {
-                $markertext .= ", {title: '{$this->markers[$i]['onClick']}'}).on('click', onClickShowDiv";
-                if($onClickFunText == '') {
-                    $onClickFunText = "function onClickShowDiv(e) { document.getElementById('onClickDiv').innerHTML= this.options.title; }\n";
-                }
+                $markertext .= ", {onClickText: '{$this->markers[$i]['onClick']}'}";
             }
-            $markertext .= ")";
-            
-            if(isset($this->markers[$i]['toolTip'])) {
-                $markertext .= ".bindTooltip('{$this->markers[$i]['toolTip']}')";
-            }
+            $markertext .= ')';
+            if(isset($this->markers[$i]['onClick'])) $this->addOnClickFunction($this->markers[$i], $markertext);
+            $this->addText($this->markers[$i], $markertext);
             $markertext .= ".addTo(map);\n";
             $scriptText .= $markertext;
-            if(count($this->markers) > 1) $drawnItems .= "marker$i,";
+            $drawnItems .= "marker$i,";
         }
 
         for($i=0; $i<count($this->circles); ++$i) {
-            $scriptText .= "var circle$i = L.circle([{$this->circles[$i]['lat']}, {$this->circles[$i]['lon']}], {color: '{$this->circles[$i]['color']}'}).addTo(map);\n";
-            if(count($this->circles) > 1) $drawnItems .= "circle$i,";
-        }
-
-        for($i=0; $i<count($this->polygons); ++$i) {
-            $polytext = "var polygon$i = L.polygon([";
-            foreach ($this->polygons[$i]['data'] as $coord) {
-                $polytext .= "[{$coord[1]}, {$coord[0]}],";
+            $circleText = "var circle$i = L.circle([{$this->circles[$i]['lat']}, {$this->circles[$i]['lon']}]";
+            if(isset($this->circles[$i]['color']) || (isset($this->circles[$i]['radius'])) || (isset($this->circles[$i]['onClick']))) {
+                $optionsText = '';
+                if(isset($this->circles[$i]['color'])) {
+                    $optionsText .= "color: '{$this->circles[$i]['color']}'";
+                }
+                if(isset($this->circles[$i]['radius'])) {
+                    if($optionsText != '') $optionsText .= ', ';
+                    $optionsText .= "radius: '{$this->circles[$i]['radius']}'";
+                }
+                if(isset($this->circles[$i]['onClick'])) {
+                    if($optionsText != '') $optionsText .= ', ';
+                    $optionsText .= "onClickText: '{$this->circles[$i]['onClick']}'";
+                }
+                $circleText .= ', {'.$optionsText.'}';
             }
-            $polytext = substr($polytext, 0, -1); //remove last ','
-            $polytext .= "], {color: '{$this->polygons[$i]['color']}'}).addTo(map);\n";
-            $scriptText .= $polytext;
+            $circleText .= ')';
+            if(isset($this->circles[$i]['onClick'])) $this->addOnClickFunction($this->circles[$i], $circleText);
+            $this->addText($this->circles[$i], $circleText);
+            $circleText .= ".addTo(map);\n";
+            $scriptText .= $circleText;
+            $drawnItems .= "circle$i,";
+        }
+        for($i=0; $i<count($this->polygons); ++$i) {
+            $polygonText = "var polygon$i = L.polygon([";
+            if(isset($this->polygons[$i]['data'])) {
+                //simple polygon
+                foreach ($this->polygons[$i]['data'] as $coord) {
+                    $polygonText .= "[{$coord[1]}, {$coord[0]}],";
+                }
+            } else if(isset($this->polygons[$i]['multi'])) {
+                //multipolygon
+                foreach ($this->polygons[$i]['multi'] as $polygon) {
+                    $polygonText .= "[";
+                    foreach ($polygon as $coord) {
+                        $polygonText .= "[{$coord[1]}, {$coord[0]}],";
+                    }
+                    $polygonText = substr($polygonText, 0, -1); //remove last ','
+                    $polygonText .= "],";
+                }
+            }
+            $polygonText = substr($polygonText, 0, -1); //remove last ','
+            $polygonText .= "]";
+            if(isset($this->polygons[$i]['color']) || (isset($this->polygons[$i]['onClick']))) {
+                $optionsText = '';
+                if(isset($this->polygons[$i]['color'])) {
+                    $optionsText .= "color: '{$this->polygons[$i]['color']}'";
+                }
+                if(isset($this->polygons[$i]['onClick'])) {
+                    if($optionsText != '') $optionsText .= ', ';
+                    $optionsText .= "onClickText: '{$this->polygons[$i]['onClick']}'";
+                }
+                $polygonText .= ', {'.$optionsText.'}';
+            }
+            $polygonText .= ')';
+            if(isset($this->polygons[$i]['onClick'])) $this->addOnClickFunction($this->polygons[$i], $polygonText);
+            $this->addText($this->polygons[$i], $polygonText);
+            $polygonText .= ".addTo(map);\n";
+            $scriptText .= $polygonText;
             $drawnItems .= "polygon$i,";
         }
 
         for($i=0; $i<count($this->geoJSONs); ++$i) {
-            $scriptText .= "L.geoJSON({$this->geoJSONs[$i]['data']}, {color: '{$this->geoJSONs[$i]['color']}'}).addTo(map);";
+            $geoJSONText = "L.geoJSON({$this->geoJSONs[$i]['data']}";
+            if(isset($this->geoJSONs[$i]['color'])) {
+                $geoJSONText .= ", {color: '{$this->geoJSONs[$i]['color']}'}";
+            }
+            $geoJSONText .= ").addTo(map);\n";
+            $scriptText .= $geoJSONText;
             $drawnItems .= "geoJSON$i,";
         }
 
@@ -149,11 +294,34 @@ class LeafletMaphp {
             $drawnItems = substr($drawnItems, 0, -1); //remove last ','
             $scriptText .= $drawnItems."]);\nmap.fitBounds(drawnItems.getBounds());\n";
         }
-        return "<div id='{$this->div_id}'></div>\n<script>$onClickFunText$scriptText</script>\n";
+
+        $theStyle = '';
+        if($this->div_style != '') {
+            $theStyle = " style='$this->div_style'";
+        }
+        return "<div id='{$this->div_id}'$theStyle></div>\n<script>{$this->onClickFunText}$scriptText</script>\n";
     }
 
     function __toString() : string {
         return $this->show();
+    }
+
+    private function addText(array $item, string &$itemText) {
+        
+        if(isset($item['toolTip'])) {
+            $itemText .= ".bindTooltip('{$item['toolTip']}')";
+        }
+        if(isset($item['popUp'])) {
+            $itemText .= ".bindPopup('{$item['popUp']}')";
+        }
+    }
+
+    private function addOnClickFunction(array $item, string &$itemText) {
+        
+        if(isset($item['onClick'])) {
+            $itemText .= ".on('click', onClickShowDiv)";
+            if($this->onClickFunText == '') $this->onClickFunText = "function onClickShowDiv(e) { document.getElementById('onClickDiv').innerHTML= this.options.onClickText; }\n";            
+        }
     }
 }
 ?>
--- README.md
+++ README.md
@@ -1,54 +1,48 @@
 LeafletMaphp
 ============
-A simple PHP wrapper to Leaflet JS
+A simple PHP wrapper to Leaflet JS using OSM data and tiles
 
 Basic usage
 -----------
 
-Create a new instance of LeafletMaphp.
+Create a new instance of LeafletMaphp. You can optionally give a name to the required div for the map, height and width in px (by default 'map' and 300), and style in a text string containing the desired css. Attribution to OpenStreetMap contributors will be added by default.
 ```php
 $map = new LeafletMaphp();
 ```
 
-Before closing the <head> tag add the required Leaflet tags (curretly for Leaflet version 1.7.1)
+Before closing the &lt;head&gt; tag add the required Leaflet tags (curretly for Leaflet version 1.7.1)
 
 ```php
 echo $map->showHeadTags();
 ```
 
-You can add several Leaflet elements using the proper method
+You can add several Leaflet elements using the proper method. Several methods have optional parameters.
+
+Markers, Circles and Polygons (including Multipolygons) will return the numeric id of the current element of its type. Circle #0 is different from Marker #0 and from Polygon #0. Please note that Polygons and Multipolygons are in the same list, so the id of a Multipolygon added after Polygon #3 will be Multipolygon #4.
+
+When adding several elements of any type, the Bounding Box is automatically recalculated for keeping all of them on sight.
 ```php
-echo $map->showHeadTags();
+$map->setCenter($lat, $lon, $bounds, $zoom);
+$map->addMarker($lat, $lon);
+$map->addCircle($lat, $lon, $color?);
+$map->addPolygon($data, $color?);
+$map->addMultipolygon($data, $color?);
+$map->addGeoJSON($data, $color?);
 ```
 
+You can add several Leaflet text elements: Popups, ToolTips and onClickText. All of them can be added in Markers, Circles and Polygons (including Multipolygons). You have to select the type using the constants defined (MARKER, CIRCLE, POLYGON and the id of the desired element)
+```php
+$map->addTooltip($element_type, $element_id, $toolTipText);
+$map->addPopUp($element_type, $element_id, $PopupText);
+$map->addOnClickText($element_type, $element_id, $onClickText);
+```
 
-    //Incluimos en el head las etiquetas necesarias
-    echo $map->showHeadTags()."\t<title>{$place['place_id']}</title>\n</head>\n<body>\n";
+Finally you have to show the &lt;div&gt; element by calling its own method where you desired
+```php
+echo $map->show();
+```
 
-    // Establecemos centro, zoom y tamaño del mapa
-    $map->setCenter($place['lat'], $place['lon'], $place["boundingbox"]);
-
-    // Añadimos un marcador en la posición obtenida
-    $map->addMarker($place['lat'], $place['lon'], $place["display_name"], "Has clicado las coordenadas {$place['lat']}, {$place['lon']}");
-
-    // Añadimos un círculo en la posición obtenida
-    $map->addCircle($place['lat'], $place['lon']);
-
-    // Añadimos un polígono que rodee el objeto si existe
-    if((isset($place['geojson'])) && ($place['geojson']['type'] == 'Polygon')) {
-        $map->addPolygon($place['geojson']['coordinates'][0]);
-    }
-    else if ($place['osm_type'] == 'relation') {
-        $geoJSON_url = "http://polygons.openstreetmap.fr/get_geojson.py?id={$place['osm_id']}";
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-        curl_setopt($ch, CURLOPT_URL, $geoJSON_url);
-        $geoJSON = curl_exec($ch);
-        curl_close($ch);
-        $map->addGeoJSON($geoJSON);
-    }
-    echo "{$place['display_name']}<br>\n";
-    // Mostramos el mapa
-    echo $map->show();
-    // Mostramos el div que se usará al pulsar el mapa
-    echo $map->showOnClickDiv();
+If you have added any onClickText on any marker, you have to show also a new &lt;div&gt; element that will have the id 'onClickDiv'
+```php
+echo $map->showOnClickDiv();
+```
